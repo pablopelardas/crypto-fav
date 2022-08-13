07a094b4f08d510c69e1c760869cb47120c7c05c
pablo pelardas <pablo.pelardas@gmail.com>
2022-08-13 15:22:55 -0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import Layout from './views/Layout'
-import Home from './views/Home'
+import React from "react";
+import Layout from "./views/Layout";
+import Home from "./views/Home";
 function App() {
-
-
   return (
     <div className="App">
       <Layout>
         <Home />
       </Layout>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-center h-24 bg-amber-900'>
-      <p className='text-amber-300 text-7xl font-bold'>CryptoFav</p>
-      <button className='absolute left-6 top-8 bg-amber-300'> {'<--'} </button>
+    <nav className="flex justify-center pr-2 items-center h-24 bg-amber-900">
+      <p className="text-amber-300 text-5xl font-bold lg:text-6xl">CryptoFav</p>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

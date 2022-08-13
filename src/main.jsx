d92b826707e055db627a5aa99a1ca6f4437dddd0
pablo pelardas <pablo.pelardas@gmail.com>
2022-08-13 15:37:45 -0300

    fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App'
-import {store} from '@/app/store/store'
-import {Provider} from 'react-redux'
-import '../dist/tailwind.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App";
+import { store } from "@/app/store/store";
+import { Provider } from "react-redux";
+import "./tailwind.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
